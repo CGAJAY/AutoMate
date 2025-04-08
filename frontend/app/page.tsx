17f37eac6002aded766dcb8d5966a9832ac8685a
Frank Mugendi <njaombefrank@gmail.com>
2025-04-08 21:23:05 +0300

    Worked on the hero section of the website

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 // import Image from "next/image";
 
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="bg-amber-300 min-h-screen">
       <Header />
+      <Hero />
     </div>
   );
 }
